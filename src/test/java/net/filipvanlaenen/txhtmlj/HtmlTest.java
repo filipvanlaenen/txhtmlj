0889ba1cd5466704f32f46fde974e34c1070e3f0
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-03-15 22:56:38 +0100

    Issue #2: Added unit tests on html

--- src/test/java/net/filipvanlaenen/txhtmlj/HtmlTest.java
+++ src/test/java/net/filipvanlaenen/txhtmlj/HtmlTest.java
@@ -9,16 +9,36 @@ import org.junit.jupiter.api.Test;
  */
 public class HtmlTest {
     /**
-     * Test verifying that a simple HTML document is exported correctly.
+     * Test verifying that a simple HTML document with head and body is exported correctly.
      */
     @Test
-    void simpleHtmlDocumentIsConvertedCorrectlyToString() {
-        Head head = new Head();
-        head.addElement(new Title("Hello, world!"));
-        Body body = new Body();
-        body.addElement(new P("Hello, world!"));
+    void simpleHtmlDocumentWithHeadAndBodyIsConvertedCorrectlyToString() {
+        Head head = new Head().addElement(new Title("Hello, world!"));
+        Body body = new Body().addElement(new P("Hello, world!"));
         Html html = new Html().addElement(head).addElement(body);
         assertEquals("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n  <head>\n    <title>Hello, world!</title>\n"
                 + "  </head>\n  <body>\n    <p>Hello, world!</p>\n  </body>\n</html>", html.asString());
+    }
+
+    /**
+     * Test verifying that a simple HTML document with body only is exported correctly.
+     */
+    @Test
+    void simpleHtmlDocumentWithBodyOnlyIsConvertedCorrectlyToString() {
+        Body body = new Body().addElement(new P("Hello, world!"));
+        Html html = new Html().addElement(body);
+        assertEquals("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n  <body>\n    <p>Hello, world!</p>\n"
+                + "  </body>\n</html>", html.asString());
+    }
+
+    /**
+     * Test verifying that a simple HTML document with head only is exported correctly.
+     */
+    @Test
+    void simpleHtmlDocumentWithHeadOnlyIsConvertedCorrectlyToString() {
+        Head head = new Head().addElement(new Title("Hello, world!"));
+        Html html = new Html().addElement(head);
+        assertEquals("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n  <head>\n    <title>Hello, world!</title>\n"
+                + "  </head>\n</html>", html.asString());
     }
 }
