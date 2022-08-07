7b2ecb6e534ed3a8008d8be2f10c39113beb2bc8
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-08-07 09:23:08 +0200

    Issue #11: Resolved a Checkstyle violation

--- src/main/java/net/filipvanlaenen/txhtmlj/A.java
+++ src/main/java/net/filipvanlaenen/txhtmlj/A.java
@@ -2,12 +2,17 @@ package net.filipvanlaenen.txhtmlj;
 
 import net.filipvanlaenen.bltxmlepj.ElementWithAttributesAndContent;
 
+/**
+ * A class representing an a element.
+ *
+ * @see <a href= "https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-a-element">4.5.1 The a
+ *      element</a>
+ */
 public final class A extends ElementWithAttributesAndContent implements FlowContent {
     /**
      * Constructs an a element with a string as its content.
      *
-     * @param content
-     *            A string.
+     * @param content A string.
      */
     public A(final String content) {
         super(content);
@@ -21,8 +26,7 @@ public final class A extends ElementWithAttributesAndContent implements FlowCont
     /**
      * Sets the hyperreference.
      *
-     * @param href
-     *            The hyperreference.
+     * @param href The hyperreference.
      * @return The instance called.
      */
     public A href(final String href) {
