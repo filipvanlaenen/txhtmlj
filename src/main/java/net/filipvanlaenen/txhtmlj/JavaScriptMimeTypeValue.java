841b6724de980f1bb50bc50d65ebb301aa47e3df
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-09-30 16:36:09 +0200

    Issue #14: Resolved Checkstyle violations

--- src/main/java/net/filipvanlaenen/txhtmlj/JavaScriptMimeTypeValue.java
+++ src/main/java/net/filipvanlaenen/txhtmlj/JavaScriptMimeTypeValue.java
@@ -2,21 +2,90 @@ package net.filipvanlaenen.txhtmlj;
 
 import net.filipvanlaenen.bltxmlepj.AttributeValueEnumeration;
 
+/**
+ * An enumeration for all the JavaScript MIME types.
+ */
 public enum JavaScriptMimeTypeValue implements AttributeValueEnumeration {
-    APPLICATION_ECMASCRIPT("application/ecmascript"), APPLICATION_JAVASCRIPT("application/javascript"),
-    APPLICATION_X_ECMASCRIPT("application/x-ecmascript"), APPLICATION_X_JAVASCRIPT("application/x-javascript"),
-    TEXT_ECMASCRIPT("text/ecmascript"), TEXT_JAVASCRIPT("text/javascript"), TEXT_JAVASCRIPT_1_0("text/javascript1.0"),
-    TEXT_JAVASCRIPT_1_1("text/javascript1.1"), TEXT_JAVASCRIPT_1_2("text/javascript1.2"),
-    TEXT_JAVASCRIPT_1_3("text/javascript1.3"), TEXT_JAVASCRIPT_1_4("text/javascript1.4"),
-    TEXT_JAVASCRIPT_1_5("text/javascript1.5"), TEXT_LIVESCRIPT("text/livescript"), TEXT_JSCRIPT("text/jscript"),
-    TEXT_X_ECMASCRIPT("text/x-ecmascript"), TEXT_X_JAVASCRIPT("text/x-javascript");
+    /**
+     * The <code>application/ecmascript</code> type.
+     */
+    APPLICATION_ECMASCRIPT("application/ecmascript"),
+    /**
+     * The <code>application/javascript</code> type.
+     */
+    APPLICATION_JAVASCRIPT("application/javascript"),
+    /**
+     * The <code>application/x-ecmascript</code> type.
+     */
+    APPLICATION_X_ECMASCRIPT("application/x-ecmascript"),
+    /**
+     * The <code>application/x-javascript</code> type.
+     */
+    APPLICATION_X_JAVASCRIPT("application/x-javascript"),
+    /**
+     * The <code>text/ecmascript</code> type.
+     */
+    TEXT_ECMASCRIPT("text/ecmascript"),
+    /**
+     * The <code>text/javascript</code> type.
+     */
+    TEXT_JAVASCRIPT("text/javascript"),
+    /**
+     * The <code>text/javascript1.0</code> type.
+     */
+    TEXT_JAVASCRIPT_1_0("text/javascript1.0"),
+    /**
+     * The <code>text/javascript1.1</code> type.
+     */
+    TEXT_JAVASCRIPT_1_1("text/javascript1.1"),
+    /**
+     * The <code>text/javascript1.2</code> type.
+     */
+    TEXT_JAVASCRIPT_1_2("text/javascript1.2"),
+    /**
+     * The <code>text/javascript1.3</code> type.
+     */
+    TEXT_JAVASCRIPT_1_3("text/javascript1.3"),
+    /**
+     * The <code>text/javascript1.4</code> type.
+     */
+    TEXT_JAVASCRIPT_1_4("text/javascript1.4"),
+    /**
+     * The <code>text/javascript1.5</code> type.
+     */
+    TEXT_JAVASCRIPT_1_5("text/javascript1.5"),
+    /**
+     * The <code>text/livescript</code> type.
+     */
+    TEXT_LIVESCRIPT("text/livescript"),
+    /**
+     * The <code>text/jscript</code> type.
+     */
+    TEXT_JSCRIPT("text/jscript"),
+    /**
+     * The <code>text/x-ecmascript</code> type.
+     */
+    TEXT_X_ECMASCRIPT("text/x-ecmascript"),
+    /**
+     * The <code>text/x-javascript</code> type.
+     */
+    TEXT_X_JAVASCRIPT("text/x-javascript");
 
+    /**
+     * The value as it should be exported to a string.
+     */
     private String stringValue;
 
-    JavaScriptMimeTypeValue(String stringValue) {
+    /**
+     * Constructor taking the string value as its parameter.
+     *
+     * @param stringValue The string value.
+     */
+    JavaScriptMimeTypeValue(final String stringValue) {
         this.stringValue = stringValue;
     }
 
+    @Override
     public String asString() {
         return stringValue;
     }
--- src/main/java/net/filipvanlaenen/txhtmlj/Script.java
+++ src/main/java/net/filipvanlaenen/txhtmlj/Script.java
@@ -2,11 +2,27 @@ package net.filipvanlaenen.txhtmlj;
 
 import net.filipvanlaenen.bltxmlepj.ElementWithAttributesAndContent;
 
-public class Script extends ElementWithAttributesAndContent implements FlowContent, MetadataContent {
-    public Script(String content) {
+/**
+ * A class representing a script.
+ *
+ * @see <a href="https://html.spec.whatwg.org/multipage/scripting.html#the-script-element">4.12.1 The script element</a>
+ */
+public final class Script extends ElementWithAttributesAndContent implements FlowContent, MetadataContent {
+    /**
+     * Constructor taking the content as its parameter.
+     *
+     * @param content The content.
+     */
+    public Script(final String content) {
         super(content);
     }
 
+    /**
+     * Sets the source.
+     *
+     * @param src The source.
+     * @return The instance called.
+     */
     public Script src(final String src) {
         addStringAttribute("src", src);
         return this;
@@ -17,6 +33,12 @@ public class Script extends ElementWithAttributesAndContent implements FlowConte
         return "script";
     }
 
+    /**
+     * Sets the type.
+     *
+     * @param javaScriptMimeTypeValue The JavaScript MIME type.
+     * @return The instance called.
+     */
     public Script type(final JavaScriptMimeTypeValue javaScriptMimeTypeValue) {
         addEnumerationAttribute("type", javaScriptMimeTypeValue);
         return this;
