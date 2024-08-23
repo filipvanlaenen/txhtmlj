5ae1bcb88e7e9787a684bbc206de38cfdef68475
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-08-23 13:18:29 +0200

    Issue #72: Added the method enumeration

---
+++ src/main/java/net/filipvanlaenen/txhtmlj/MethodValue.java
@@ -0,0 +1,21 @@
+package net.filipvanlaenen.txhtmlj;
+
+import net.filipvanlaenen.bltxmlepj.KebabCaseAttributeValueEnumeration;
+
+/**
+ * An enumeration for all the method values.
+ */
+public enum MethodValue implements KebabCaseAttributeValueEnumeration {
+    /**
+     * The <code>dialog</code> value.
+     */
+    DIALOG,
+    /**
+     * The <code>get</code> value.
+     */
+    GET,
+    /**
+     * The <code>post</code> value.
+     */
+    POST;
+}
